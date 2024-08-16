fd7cfa848c19e455050505b9dd005d171782e477
alichdrdzn <85154472+alichdrdzn@users.noreply.github.com>
2024-08-16 22:22:06 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update && apt install tcpdump htop build-essential netfilter-persistent speedtest-cli snapd dnsutils -y
+apt update && apt install tcpdump htop build-essential netfilter-persistent speedtest-cli snapd dnsutils haproxy -y
 wget http://www.softether-download.com/files/softether/v4.42-9798-rtm-2023.06.30-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.42-9798-rtm-2023.06.30-linux-x64-64bit.tar.gz
 wget https://github.com/alichdrdzn/soft-down/blob/main/vpn_server.config
 tar xvf softether-vpnserver-v4.42-9798-rtm-2023.06.30-linux-x64-64bit.tar.gz
@@ -7,12 +7,14 @@ cd vpnserver
 make
 cp /root/vpn_server.config /root/vpnserver/
 chmod 600 * && chmod 700 vpnserver && chmod 700 vpncmd
+wget https://raw.githubusercontent.com/alichdrdzn/soft-down/main/install.sh
+cat haproxy.cfg /etc/haproxy/haproxy.cfd
+systemctl disable haproxy && systemctl stop haproxy
 ./vpnserver start
 sleep 2
 netstat -ntpl
 echo "@reboot /root/vpnserver/vpnserver start" >> /var/spool/cron/crontabs/root
 echo "Port 17971" >> /etc/ssh/sshd_config
-systemctl restart sshd
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -s localhost -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
@@ -23,4 +25,5 @@ iptables -A INPUT -p tcp -m tcp --dport 17971 -j ACCEPT
 iptables -P INPUT DROP
 service netfilter-persistent save
 netfilter-persistent save
+systemctl restart sshd
 echo -e "\ndone."
